d1d08dfcc4768ddb2270fd00edf8fb228bf2aaaf
mobina-rashidi <mobina.rashidi03@gmail.com>
2026-01-04 14:12:57 +0330

    add item entry page and fix tailwind v4 config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ItemEntryView from './View/pages/ItemEntry/ItemEntryView'
 import MapView from './View/pages/MapView/MapView'
 import Navbar from './View/components/navbar/Navbar'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter> 
+    <BrowserRouter>
       <Navbar />
-      <MapView />
+      <Routes>
+        <Route path="/" element={<ItemEntryView />} />
+        <Route path="/map" element={<MapView />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
