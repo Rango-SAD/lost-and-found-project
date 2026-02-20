6c13f9797cd3b4043e8f576f3c78b1329e4cd663
farbodft <farbod.ft2005@gmail.com>
2026-02-20 22:49:31 +0330

    #27 docker files updated

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,15 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware 
-from src.api.routes.category_routes import router as category_router
-from src.api.routes.post_routes import router as post_router
 from motor.motor_asyncio import AsyncIOMotorClient
-from src.infrastructure.database.models.category_document import CategoryDocument
-from src.infrastructure.database.models.post_document import PostDocument
 from beanie import init_beanie
 
-app = FastAPI(title="Lost and Found University System") 
+from src.api.routes.category_routes import router as category_router
+from src.api.routes.post_routes import router as post_router
+from src.infrastructure.database.models.category_document import CategoryDocument
+from src.infrastructure.database.models.post_document import PostDocument
+
+app = FastAPI(title="ُSharifJoo") 
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,9 +24,18 @@ app.include_router(post_router)
 
 @app.on_event("startup")
 async def auth_db():
-    client = AsyncIOMotorClient("mongodb://localhost:27017")
-    await init_beanie(database=client.lost_and_found_v2, document_models=[CategoryDocument,
-        PostDocument])
+    mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    mongo_db = os.getenv("MONGO_DB", "lost_and_found_v2")
+
+    client = AsyncIOMotorClient(mongo_uri)
+
+    await init_beanie(
+        database=client[mongo_db],
+        document_models=[
+            CategoryDocument,
+            PostDocument,
+        ],
+    )
 
 
 @app.get("/")
